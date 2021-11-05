f38b377cddd2bc3c173aca0e15b75c72437d9723
Phil Kedy <phil.kedy@gmail.com>
2021-11-05 10:33:32 -0400

    Tests for codec/json and codec/msgpack

---
+++ codec/msgpack/msgpack_test.go
@@ -0,0 +1,29 @@
+package msgpack_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/nanobus/nanobus/codec/msgpack"
+)
+
+func TestCodec(t *testing.T) {
+	name, loader := msgpack.MsgPack()
+	assert.Equal(t, "msgpack", name)
+	c, err := loader(nil, nil)
+	require.NoError(t, err)
+	assert.Equal(t, "application/msgpack", c.ContentType())
+	data := map[string]interface{}{
+		"int":    int64(1234),
+		"string": "1234",
+	}
+	encoded, err := c.Encode(data)
+	require.NoError(t, err)
+	_, _, err = c.Decode([]byte{})
+	assert.Error(t, err)
+	decoded, _, err := c.Decode(encoded)
+	require.NoError(t, err)
+	assert.Equal(t, data, decoded)
+}
