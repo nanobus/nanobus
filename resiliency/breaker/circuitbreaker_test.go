f0238d4ad203837b7af7f9dc244acbca8bec8d70
Phil Kedy <phil.kedy@gmail.com>
2021-11-12 09:09:07 -0500

    Misc tests

---
+++ resiliency/breaker/circuitbreaker_test.go
@@ -0,0 +1,31 @@
+package breaker_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/nanobus/nanobus/expr"
+	"github.com/nanobus/nanobus/resiliency/breaker"
+)
+
+func TestCircuitBreaker(t *testing.T) {
+	var trip expr.ValueExpr
+	err := trip.DecodeString("consecutiveFailures > 2")
+	require.NoError(t, err)
+	cb := breaker.CircuitBreaker{
+		Trip: &trip,
+	}
+	cb.Initialize("test")
+	for i := 0; i < 3; i++ {
+		cb.Execute(func() error {
+			return errors.New("test")
+		})
+	}
+	err = cb.Execute(func() error {
+		return nil
+	})
+	assert.EqualError(t, err, "circuit breaker is open")
+}
